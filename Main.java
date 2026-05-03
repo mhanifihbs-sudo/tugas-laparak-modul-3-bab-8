800f247a7d98b72687df616900c4a5f6966d90e8
mhanifihbs-sudo <mhanifihbs@gmail.com>
2026-05-03 19:58:02 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Manusia saya = new Manusia("Munif", 20); // Ganti dengan nama dan umur Anda
+        
+        saya.tampilkanNama();
+        saya.tampilkanUmur();
+        saya.makan();
+    }
+}
